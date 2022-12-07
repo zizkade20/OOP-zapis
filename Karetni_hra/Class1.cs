3aefcd8a00f6321b0b094924c8551df0c28e61f0
Denis Žižka <zizkade20@sps-prosek.cz>
2022-12-07 14:23:12 +0100

    Přidejte soubory projektu.

---
+++ Karetni_hra/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    internal class Zak
+    {
+        internal string Jmeno;
+        internal string Prijmeni;
+        internal int RokNarozeni;
+        //int RodneCislo;
+
+        internal Zak() { }
+        internal Zak(string _jmeno, string _prijmeni, int _rok_narozeni)
+        {
+            this.Jmeno = _jmeno;
+            this.Prijmeni = _prijmeni;
+            this.RokNarozeni = _rok_narozeni;
+        }
+
+        private int VratVek()
+        {
+            return 2022 - RokNarozeni;
+        }
+
+        internal void Pozdrav()
+        {
+            Console.WriteLine("Ahoj, já jsem " + Jmeno + " a je mi " + VratVek().ToString() + " let");
+        }
+    }
+}
---
+++ Karetni_hra/Program.cs
@@ -0,0 +1,31 @@
+﻿using oop;
+using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main()
+        {
+            // ...
+
+            Zak Zak1 = new Zak();
+            Zak1.Jmeno = "Peter";
+            Zak1.Prijmeni = "Kubín";
+            Zak1.RokNarozeni = 2004;
+
+            Zak Zak2 = new Zak()
+            {
+                Jmeno = "Generál",
+                Prijmeni = "Pavel",
+                RokNarozeni = 1980
+            };
+
+            Zak zak3 = new Zak(
+                "George",
+                "Oracle",
+                1221);
+            Zak1.Pozdrav();
+        }
+    }
+}
